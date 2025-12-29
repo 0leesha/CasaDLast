c15b8ccf840c80219c900441621231c0d6b91297
0leesha <leeshachavda5@gmail.com>
2025-12-29 21:27:26 +0530

    Delete customer directory

--- customer/JS/common.js
+++
@@ -1,7 +0,0 @@
-fetch("/customer/pages/nav.html")
-  .then(res => res.text())
-  .then(data => document.getElementById("navbar").innerHTML = data);
-
-fetch("/customer/pages/footer.html")
-  .then(res => res.text())
-  .then(data => document.getElementById("footer").innerHTML = data);
